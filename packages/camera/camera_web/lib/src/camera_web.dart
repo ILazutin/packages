19e45fd9868616903b038818727030db75cf6d62
Ilya Lazutin <lazutin.ilya@gmail.com>
2023-03-15 23:53:10 +0300

    Reland from https://github.com/ILazutin/plugins

--- packages/camera/camera_web/lib/src/camera_web.dart
+++ packages/camera/camera_web/lib/src/camera_web.dart
@@ -201,7 +201,8 @@ class CameraPlugin extends CameraPlatform {
   @override
   Future<int> createCamera(
     CameraDescription cameraDescription,
-    ResolutionPreset? resolutionPreset, {
+    ResolutionPreset? resolutionPreset,
+    ResolutionAspectRatio? resolutionAspectRatio, {
     bool enableAudio = false,
   }) =>
       createCameraWithSettings(
