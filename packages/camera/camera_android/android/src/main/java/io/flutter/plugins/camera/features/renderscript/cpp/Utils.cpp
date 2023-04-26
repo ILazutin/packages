de1466103d396a371b5b032d4de6747b7aae2dba
Ilya Lazutin <lazutin.ilya@gmail.com>
2023-04-26 10:33:40 +0300

    fix release build

--- packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/features/renderscript/cpp/Utils.cpp
+++ packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/features/renderscript/cpp/Utils.cpp
@@ -16,8 +16,6 @@
 
 #include "Utils.h"
 
-#include <cpu-features.h>
-
 #include "RenderScriptToolkit.h"
 
 namespace renderscript {
@@ -25,21 +23,6 @@ namespace renderscript {
 #define LOG_TAG "renderscript.toolkit.Utils"
 
 bool cpuSupportsSimd() {
-    AndroidCpuFamily family = android_getCpuFamily();
-    uint64_t features = android_getCpuFeatures();
-
-    if (family == ANDROID_CPU_FAMILY_ARM && (features & ANDROID_CPU_ARM_FEATURE_NEON)) {
-        // ALOGI("Arm with Neon");
-        return true;
-    } else if (family == ANDROID_CPU_FAMILY_ARM64 && (features & ANDROID_CPU_ARM64_FEATURE_ASIMD)) {
-        // ALOGI("Arm64 with ASIMD");
-        return true;
-    } else if ((family == ANDROID_CPU_FAMILY_X86 || family == ANDROID_CPU_FAMILY_X86_64) &&
-               (features & ANDROID_CPU_X86_FEATURE_SSSE3)) {
-        // ALOGI("x86* with SSE3");
-        return true;
-    }
-    // ALOGI("Not simd");
     return false;
 }
 
