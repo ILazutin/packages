89fe06d4968de345dbeb40be0124f5f5bea469a0
Ilya Lazutin <lazutin.ilya@gmail.com>
2023-07-17 12:10:18 +0300

    refactor yuv to nv converting

--- packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/ImageUtils.java
+++ packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/ImageUtils.java
@@ -53,61 +53,109 @@ public class ImageUtils {
     }
 
     public static byte[] YUV420toNV21(Image image) {
-        if (image == null) return null;
+        if (image == null || image.getFormat() != ImageFormat.YUV_420_888) return null;
 
-        Rect crop = image.getCropRect();
-        int format = image.getFormat();
-        int width = crop.width();
-        int height = crop.height();
+        Rect imageCrop = image.getCropRect();
         Image.Plane[] planes = image.getPlanes();
-        byte[] data = new byte[width * height * ImageFormat.getBitsPerPixel(format) / 8];
-        byte[] rowData = new byte[planes[0].getRowStride()];
+        int pixelCount = imageCrop.width() * imageCrop.height();
 
-        int channelOffset = 0;
-        int outputStride = 1;
+        byte[] outputBuffer = new byte[pixelCount * ImageFormat.getBitsPerPixel(ImageFormat.YUV_420_888) / 8];
+
         for (int i = 0; i < planes.length; i++) {
+            // How many values are read in input for each output value written
+            // Only the Y plane has a value for every pixel, U and V have half the resolution i.e.
+            //
+            // Y Plane            U Plane    V Plane
+            // ===============    =======    =======
+            // Y Y Y Y Y Y Y Y    U U U U    V V V V
+            // Y Y Y Y Y Y Y Y    U U U U    V V V V
+            // Y Y Y Y Y Y Y Y    U U U U    V V V V
+            // Y Y Y Y Y Y Y Y    U U U U    V V V V
+            // Y Y Y Y Y Y Y Y
+            // Y Y Y Y Y Y Y Y
+            // Y Y Y Y Y Y Y Y
+            int outputStride = 0;
+
+            // The index in the output buffer the next value will be written at
+            // For Y it's zero, for U and V we start at the end of Y and interleave them i.e.
+            //
+            // First chunk        Second chunk
+            // ===============    ===============
+            // Y Y Y Y Y Y Y Y    V U V U V U V U
+            // Y Y Y Y Y Y Y Y    V U V U V U V U
+            // Y Y Y Y Y Y Y Y    V U V U V U V U
+            // Y Y Y Y Y Y Y Y    V U V U V U V U
+            // Y Y Y Y Y Y Y Y
+            // Y Y Y Y Y Y Y Y
+            // Y Y Y Y Y Y Y Y
+            int outputOffset = 1;
+
             switch (i) {
                 case 0:
-                    channelOffset = 0;
                     outputStride = 1;
+                    outputOffset = 0;
                     break;
                 case 1:
-                    channelOffset = width * height + 1;
                     outputStride = 2;
+                    outputOffset = pixelCount + 1;
                     break;
                 case 2:
-                    channelOffset = width * height;
                     outputStride = 2;
+                    outputOffset = pixelCount;
                     break;
             }
 
-            ByteBuffer buffer = planes[i].getBuffer();
+            ByteBuffer planeBuffer = planes[i].getBuffer();
             int rowStride = planes[i].getRowStride();
             int pixelStride = planes[i].getPixelStride();
 
-            int shift = (i == 0) ? 0 : 1;
-            int w = width >> shift;
-            int h = height >> shift;
-            buffer.position(rowStride * (crop.top >> shift) + pixelStride * (crop.left >> shift));
-            for (int row = 0; row < h; row++) {
-                int length;
+            // We have to divide the width and height by two if it's not the Y plane
+            Rect planeCrop = imageCrop;
+            if (i != 0) {
+                planeCrop = new Rect(imageCrop.left / 2, imageCrop.top / 2, imageCrop.right / 2, imageCrop.bottom / 2);
+            }
+
+            int planeWidth = planeCrop.width();
+            int planeHeight = planeCrop.height();
+
+            // Intermediate buffer used to store the bytes of each row
+            byte[] rowBuffer = new byte[planes[i].getRowStride()];
+
+            int rowLength = 0;
+            if (pixelStride == 1 && outputStride == 1) {
+                rowLength = planeWidth;
+            } else {
+                // Take into account that the stride may include data from pixels other than this
+                // particular plane and row, and that could be between pixels and not after every
+                // pixel:
+                //
+                // |---- Pixel stride ----|                    Row ends here --> |
+                // | Pixel 1 | Other Data | Pixel 2 | Other Data | ... | Pixel N |
+                //
+                // We need to get (N-1) * (pixel stride bytes) per row + 1 byte for the last pixel
+                rowLength = (planeWidth - 1) * pixelStride + 1;
+            }
+
+            for (int row = 0; row < planeHeight; row++) {
+                // Move buffer position to the beginning of this row
+                planeBuffer.position(
+                        (row + planeCrop.top) * rowStride + planeCrop.left * pixelStride);
+
                 if (pixelStride == 1 && outputStride == 1) {
-                    length = w;
-                    buffer.get(data, channelOffset, length);
-                    channelOffset += length;
+                    // When there is a single stride value for pixel and output, we can just copy
+                    // the entire row in a single step
+                    planeBuffer.get(outputBuffer, outputOffset, rowLength);
+                    outputOffset += rowLength;
                 } else {
-                    length = (w - 1) * pixelStride + 1;
-                    buffer.get(rowData, 0, length);
-                    for (int col = 0; col < w; col++) {
-                        data[channelOffset] = rowData[col * pixelStride];
-                        channelOffset += outputStride;
+                    // When either pixel or output have a stride > 1 we must copy pixel by pixel
+                    planeBuffer.get(rowBuffer, 0, rowLength);
+                    for (int col = 0; col < planeWidth; col++) {
+                        outputBuffer[outputOffset] = rowBuffer[col * pixelStride];
+                        outputOffset += outputStride;
                     }
-                }
-                if (row < h - 1) {
-                    buffer.position(buffer.position() + rowStride - length);
                 }
             }
         }
-        return data;
+        return outputBuffer;
     }
 }
